f7818070753eb4f0b289cc2f5c632f79a5bfcc8d
mohamed <mohamedkharashy27@yahoo.com>
2023-06-08 11:47:55 +0000

    1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
-def main():
-    x = dir(hidden_4)
-    for a in range(len(x)):
-        if (x[a][0] != '_'):
-            print("{}".format(x[a]))
+if __name__ == "__main__":
+	import hidden_4
 
-            if __name__ == "__main__":
-                main()
+	values = dir(hidden_4)
+	for value in values:
+		if value[:2] != "__":
+			print(value)
