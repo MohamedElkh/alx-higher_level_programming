306d4eeabcb77b057350dadb84f1eabd35ccebdc
mohamed <mohamedkharashy27@yahoo.com>
2023-06-10 09:27:47 -0700

    1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	if matrix:
-		for els in matrix:
-			x = 1
-			lon = len(els)
+    if matrix:
+        for els in matrix:
+            x = 1
+            lon = len(els)
 
-			for el in els:
-				if x == lon:
-					print("{:d}".format(el), end='')
-				else:
-					print("{:d}".format(el), end=' ')
-				x += 1
-			print()
-
-			
+            for el in els:
+                if x == lon:
+                    print("{:d}".format(el), end='')
+                else:
+                    print("{:d}".format(el), end=' ')
+                x += 1
+            print()
