8845b6492b5aea4e8a3acbbd7a550f03e514f1d6
mohamed <mohamedkharashy27@yahoo.com>
2023-06-11 01:49:00 -0700

    1

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ * print_python_list_info - function prints some basic info Python lists.
+ * @p: the pointer to be connected
+ *
+ * Return: nothing.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	PyObject *elem;
+	int x, curr;
+	int length_of_list;
+	length_of_list = Py_SIZE(p);
+	curr = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", length_of_list);
+	printf("[*] Allocated = %d\n", curr);
+
+	for (x = 0; x < length_of_list; x++)
+	{
+		printf("Element %d: ", x);
+		elem = PyList_GetItem(p, x);
+		print("%s\n", x, Py_TYPE(elem)->tp_name);
+	}
+}
