482bac553f94dfa63c91571b29e7af196b065774
mohamed <mohamedkharashy27@yahoo.com>
2023-06-11 00:14:55 -0700

    1

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-	another_list = []
-	for x in my_list:
-		if x % 2 == 0:
-			another_list.append(True)
-		else:
-			another_list.append(False)
-	return another_list
+    another_list = []
+    for x in my_list:
+        if x % 2 == 0:
+            another_list.append(True)
+        else:
+            another_list.append(False)
+    return another_list
