b584c8aa445e661763864f4aaf356423607024bc
mohamed <mohamedkharashy27@yahoo.com>
2023-06-14 11:10:08 +0000

    1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
-       	return (0)
+        return (0)
 
-     vg = 0
-     value = 0
-     for t in my_list:
-         vg += (t[0] * t[1])
-	 value += t[1]
-     return (vg /value)		
+    vg = 0
+    value = 0
+
+    for tu in my_list:
+        vg += (tu[0] * tu[1])
+        value += tu[1
+]
+    return (vg / value)
