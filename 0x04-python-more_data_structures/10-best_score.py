dd3a5da1dbf4e2cfab51c54cd5b9b80b849674ba
mohamed <mohamedkharashy27@yahoo.com>
2023-06-14 09:51:55 -0700

    1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
 
     mx_dic = max(a_dictionary.values(), default=None)
-    
+
     for x, z in a_dictionary.items():
         if z == mx_dic:
-            return x	
+            return x
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dic = {}
     for x in a_dictionary:
         new_dic[x] = a_dictionary[x] * 2
-    return new_dic		
+    return new_dic
