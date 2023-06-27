31604383b7946cb30f5f638aa75e3c161aeddad4
mohamed <mohamedkharashy27@yahoo.com>
2023-06-27 01:15:54 -0700

    1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 class Node:
 
     def __init__(self, data, next_node=None):
@@ -9,54 +10,71 @@ class Node:
 
     @property
     def data(self):
+
         return (self.__data)
 
     @data.setter
     def data(self, value):
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
+
         self.__data = value
 
     @property
     def next_node(self):
+
         return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
+
         if not isinstance(value, Node) and value is not None:
+
             raise TypeError("next_node must be a Node object")
+
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    
+
     def __init__(self):
+
         self.__head = None
 
     def sorted_insert(self, value):
 
-        new = Node(value)
+        newvalue = Node(value)
 
         if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
+            newvalue.next_node = None
+            self.__head = newvalue
 
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+        elif self.__head.data > value:
+
+            newvalue.next_node = self.__head
+            self.__head = newvalue
+
+        else:
+            tmpvalue = self.__head
+
+            while (tmpvalue.next_node is not None and
+                    tmpvalue.next_node.data < value):
+
+                tmpvalue = tmpvalue.next_node
+            newvalue.next_node = tmpvalue.next_node
+
+            tmpvalue.next_node = newvalue
 
     def __str__(self):
- 
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+
+        valuess = []
+
+        tmpvalue = self.__head
+
+        while tmpvalue is not None:
+
+            valuess.append(str(tmpvalue.data))
+            tmpvalue = tmpvalue.next_node
+
+        return ('\n'.join(valuess))
