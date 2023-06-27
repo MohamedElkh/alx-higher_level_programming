d72d9a1f43968ba9f91220e4082a99f84ec3e42c
mohamed <mohamedkharashy27@yahoo.com>
2023-06-27 07:57:42 -0700

    1

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+""" define magic class and use the math """
+
+
 import math
+""" define the magicclass """
 
 
 class MagicClass:
+    """ represent the class magic """
 
     def __init__(self, radius=0):
+        """ initialize the magicclass
+
+        Args:
+           radius: check if it not int and not float
+        """
 
         self.__radius = 0
 
@@ -14,10 +24,10 @@ class MagicClass:
 
         self.__radius = radius
 
-    def area(self):
-
-        return (self.__radius ** 2 * math.pi)
-
     def circumference(self):
-
+        """ func to return the value of magicclass """
         return (2 * math.pi * self.__radius)
+
+    def area(self):
+        """ func to return the area of magicclass """
+        return (self.__radius ** 2 * math.pi)
