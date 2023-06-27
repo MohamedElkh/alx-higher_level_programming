2f48e3e7438ad9287b3b2598bcca6852ff74ebf2
mohamed <mohamedkharashy27@yahoo.com>
2023-06-27 07:17:09 -0700

    1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
+""" define the class square """
 
 
 class Node:
+    """ represent the class square """
 
     def __init__(self, data, next_node=None):
+        """ initialize the class square
+        Args:
+            initialize the data
+
+            initialize the next_node
+        """
 
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-
+        """ get the current value of data """
         return (self.__data)
 
     @data.setter
     def data(self, value):
-
+        """ set the current value of data"""
         if not isinstance(value, int):
+
             raise TypeError("data must be an integer")
 
         self.__data = value
 
     @property
     def next_node(self):
-
+        """ get the current value of next_node """
         return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
-
+        """ set the value of next_node """
         if not isinstance(value, Node) and value is not None:
 
             raise TypeError("next_node must be a Node object")
@@ -37,9 +46,10 @@ class Node:
 
 
 class SinglyLinkedList:
+    """ define the class singly linkedlist """
 
     def __init__(self):
-
+        """ initialize the class singlylinkedlist """
         self.__head = None
 
     def sorted_insert(self, value):
