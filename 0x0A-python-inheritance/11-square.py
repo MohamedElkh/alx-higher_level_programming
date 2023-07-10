fa07e6bcace025952316d4a542f42497eede21ba
mohamed <mohamedkharashy27@yahoo.com>
2023-07-10 08:03:04 -0700

    1

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ deifne the rect """
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Square(Rectangle):
@@ -16,3 +17,6 @@ class Square(Rectangle):
         super().__init__(size, size)
 
         self.__size = size
+
+    def __str__(self):
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
