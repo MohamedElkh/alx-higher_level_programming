2815d720573cae59af0c46969b8411f8e4584046
mohamed <mohamedkharashy27@yahoo.com>
2023-07-11 06:28:42 -0700

    1

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ define the model json """
-import json
+from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
     """ function object to text file """
-    with open(filename, "w") as fl:
-        json.dumb(my_obj, fl)
+    with open(filename, mode='w', encoding='utf-8') as fl:
+        fl.write(dumps(my_obj))
