cf0f6085dbd40e8cb865932c2b191af2884239e0
mohamed <mohamedkharashy27@yahoo.com>
2023-08-15 07:07:52 -0700

    1

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,7 @@
 -- creates and fills a table secondtable with attr id, name and scor in sql server
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+-- database in sql server
+-- adds rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
