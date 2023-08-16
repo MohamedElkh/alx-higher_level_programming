9c01b387659d879448ccf4f6bc9ba77332531611
mohamed <mohamedkharashy27@yahoo.com>
2023-08-16 05:43:24 -0700

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates sql server with user user_0d_1
 -- set a password for user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON  *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON  *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
