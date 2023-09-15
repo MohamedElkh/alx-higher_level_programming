2a16c404baf2a8bef58b7b0de18e8d16e2c52b9d
mohamed <mohamedkharashy27@yahoo.com>
2023-09-15 08:49:55 -0700

    1

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     curr.execute("SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id;")
 
-    states = curr.fetchall()
+    stat = curr.fetchall()
 
-    for st in states:
+    for st in stat:
         print(st)
