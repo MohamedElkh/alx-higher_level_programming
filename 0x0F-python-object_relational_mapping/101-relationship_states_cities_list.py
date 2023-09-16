7eaa3138ca81c5008afbde5b604c08f083293cc4
mohamed <mohamedkharashy27@yahoo.com>
2023-09-16 08:23:53 -0700

    1

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
                          sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
 
-    Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     see = Session()
 
