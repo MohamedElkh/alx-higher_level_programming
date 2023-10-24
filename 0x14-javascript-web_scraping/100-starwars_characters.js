382db6a37fc563c643785037cc6cf457af79c15a
mohamed <mohamedkharashy27@yahoo.com>
2023-10-24 01:38:53 -0700

    1

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+
+  const data = JSON.parse(body);
+  const datax = data.characters;
+
+  for (const x of datax) {
+    req.get(x, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const dataxx = JSON.parse(body1);
+      console.log(dataxx.name);
+    });
+  }
+});
